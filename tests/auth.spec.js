1f0d68a9234a2b079907e608496ded7962b70603
ProTasiko <prodan.anastasiia@gmail.com>
2023-04-28 12:05:03 -0500

    add positive tests to auth spec

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -19,5 +19,28 @@ describe('Authorization tests', () => {
                 .send({email: "akuna@matata.com", password: "AkunaMatata123"})
             expect(res.body.message).to.eq('Auth success')
         })
+
+        it('Response has a token', async () => {
+            let res= await request('https://clientbase-server.herokuapp.com')
+                .post('/v5/user/login')
+                .send({email: "akuna@matata.com", password: "AkunaMatata123"})
+            expect(res.body.payload.token).to.be.a('string')
+        })
+
+        it('Response body contains user ID', async() => {
+            let res= await request('https://clientbase-server.herokuapp.com')
+                .post('/v5/user/login')
+                .send({email: "akuna@matata.com", password: "AkunaMatata123"})
+            expect(res.body.payload.userId).to.be.a('string')
+        })
+
+        it('Response does not contain password', async () => {
+            let res= await request('https://clientbase-server.herokuapp.com')
+                .post('/v5/user/login')
+                .send({email: "akuna@matata.com", password: "AkunaMatata123"})
+            expect(res).to.not.have.property('password')
+        })
     })
+
+    describe('Authentication tests negative')
 })
