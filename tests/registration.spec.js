23cf7f168a93324a0e30cfeb583e2f6cb83bed75
ProTasiko <prodan.anastasiia@gmail.com>
2023-05-15 12:17:03 -0500

    test added

--- tests/registration.spec.js
+++ tests/registration.spec.js
@@ -1,22 +1,30 @@
 import request from "supertest";
 import { faker } from '@faker-js/faker';
+import {expect} from "chai";
 
 describe('Registration positive',  () => {
     describe("Registration with valid credentials", () => {
-        let res
+        let response
         const companyName = faker.company.name()
         const firstName = faker.person.firstName()
         const lastName = faker.person.lastName()
-        const email = Date.now() + 'gmail.com'
+        const email = faker.internet.email()
         const password = faker.internet.password()
-        const ver = 'v5'
-
 
         before(async () => {
-            res = await request(process.env.BASE_URL)
+            response = await request(process.env.BASE_URL)
               .post('/v5/user')
-              .send({companyName, firstName, lastName, email, password, ver})
+              .send({companyName, firstName, lastName, email, password})
         })
-            it('check status code')
+        // console.log(companyName, firstName, lastName, email, password)
+            it('check status code', () => {
+                expect(response.statusCode).to.eq(201)
+            })
+
+        it('Response status message is correct', () => {
+            expect(response.body.message).to.eq(
+                'User created successfully. Please check your email and verify it')
+        })
+
     })
 })
